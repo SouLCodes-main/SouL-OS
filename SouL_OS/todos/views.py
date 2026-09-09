a0eb9ebe657bb9303552a2574b27e86496f33b27
Prabal Singh <prabalpsingh2429@gmail.com>
2026-09-10 01:09:46 +0530

    tasks display fixed

--- SouL_OS/todos/views.py
+++ SouL_OS/todos/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from flask import request
 
 # Create your views here.
 from .models import Task
@@ -7,5 +6,5 @@ from .models import Task
 def task_list(request):
     tasks = Task.objects.all().order_by('due_date')
 
-    task_table = {'tasks' : tasks}
-    return render(request, 'task_list.html', task_table)
+    context = {'tasks' : tasks}
+    return render(request, 'task_list.html', context)
