d58795e98ba82288cf7026047b4e1f7dfa960ad5
Prabal Singh <prabalpsingh2429@gmail.com>
2026-09-15 21:53:38 +0530

    added complete/ delete task actions

--- SouL_OS/todos/forms.py
+++ SouL_OS/todos/forms.py
@@ -4,4 +4,4 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description'] # We exclude is_completed since new tasks are pending by default
+        fields = ['title', 'description'] 
--- SouL_OS/todos/views.py
+++ SouL_OS/todos/views.py
@@ -1,22 +1,31 @@
-from django.shortcuts import redirect, render
-
-# Create your views here.
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Task
 from .forms import TaskForm
 
 def task_list(request):
-    # Handle the form submission
+    # form submission
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('task_list') # Refresh the page to show the new task
     
-    # If it's a GET request, just show the empty form
+    # Get request
     else:
         form = TaskForm()
     tasks = Task.objects.all().order_by('due_date')
 
     context = {'tasks' : tasks, 'form': form}
     return render(request, 'task_list.html', context)
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.is_completed = not task.is_completed 
+    task.save()
+    return redirect('task_list')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('task_list')
 
